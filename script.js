4a736bb1231fef981cbb32a7bd7266b009bdaa24
AlexSinos <alexissinos@gmail.com>
2024-06-08 17:35:41 +0100

    New Features: add button to clear board color selector change layout add title
    Bug Fixes:
    fix opacity error

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 let container = document.querySelector(".container")
 let numDivs = 0
-let button = document.querySelector("button")
+let changeSizeButton = document.querySelector("#change-size-button")
+let clearButton = document.querySelector("#clear-button")
 let gridSize = 16
+let color = ""
+let colorButton = document.querySelectorAll(".colors > button")
 
-button.addEventListener("click", () => {
-    const divBoxes = document.querySelectorAll(".grid-boxes")
-    divBoxes.forEach(div => div.remove())
+changeSizeButton.addEventListener("click", () => {
     gridSize = parseInt(prompt("How big would you like each side to be?"))
     buildBox()
 })
+clearButton.addEventListener("click", () => {
+    confirm("Are you sure you want to clear the board?") ? buildBox() : null; 
+})
+colorButton.forEach(button => {
+    button.addEventListener("click", () => {
+    color = button.id
+})})
 
 function buildBox() {
+    const divBoxes = document.querySelectorAll(".grid-boxes")
+    divBoxes.forEach(div => div.remove())
     let numDivs = 0;
     while (numDivs < gridSize*gridSize) {
         const div = document.createElement("div");
@@ -22,18 +32,18 @@ function buildBox() {
     }
     addEventListenerToBoxes()
 }
-
 function addEventListenerToBoxes() {
     let gridBoxes = document.querySelectorAll(".grid-boxes")
     gridBoxes.forEach(box => {
         let opacity = 0
         box.addEventListener("mouseenter", () => {
-            box.style.backgroundColor = ("black") 
+            box.style.backgroundColor = color 
+            opacity += 15 
             box.style.opacity = `${opacity}%`
-            opacity += 10 
         })
     })
 }
+
 buildBox()
 
 
